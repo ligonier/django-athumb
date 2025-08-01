aff72d91e4cb77437b8b41f998d139c262b90844
Seth White <seth.loren.white.2014@gmail.com>
2025-08-01 17:28:40 -0500

    chore(project): convert to uv

--- setup.py
+++ setup.py
@@ -1,29 +1,33 @@
-from distutils.core import setup
+from setuptools import setup
+
 import athumb
 
-long_description = open('README.rst').read()
+long_description = open("README.md").read()
 
 setup(
-    name='django-athumb',
+    name="django-athumb",
     version=athumb.VERSION,
-    packages=['athumb', 'athumb.backends', 'athumb.management',
-              'athumb.management.commands', 'athumb.templatetags',
-              'athumb.upload_handlers'],
-    description='A simple, S3-backed thumbnailer field.',
+    packages=[
+        "athumb",
+        "athumb.management",
+        "athumb.management.commands",
+        "athumb.templatetags",
+    ],
+    description="A simple, S3-backed thumbnailer field.",
     long_description=long_description,
-    author='Gregory Taylor',
-    author_email='gtaylor@duointeractive.com',
-    license='BSD License',
-    url='http://github.com/duointeractive/django-athumb',
+    author="Gregory Taylor",
+    author_email="gtaylor@duointeractive.com",
+    license="BSD License",
+    url="https://github.com/duointeractive/django-athumb",
     platforms=["any"],
-    requires=['django', 'boto', 'pil'],
+    install_requires=["django", "django-storages[s3]", "pillow"],
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Natural Language :: English',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Environment :: Web Environment',
+        "Development Status :: 5 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Environment :: Web Environment",
     ],
 )
