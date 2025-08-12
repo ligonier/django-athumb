93b1cb3949c23c7ff5b8d11a4282d650020941af
Seth White <seth.loren.white.2014@gmail.com>
2025-08-12 11:11:07 -0500

    chore(regen script): make sure to only re-generate images that are missing

--- athumb/management/commands/athumb_regen_field.py
+++ athumb/management/commands/athumb_regen_field.py
@@ -15,10 +15,16 @@ class Command(BaseCommand):
         parser.add_argument(
             "field_name", nargs=1, help='The field name, such as "image"'
         )
+        parser.add_argument(
+            "--force",
+            action="store_true",
+            help="Force regeneration of all thumbnails, even if they exist"
+        )
 
     def handle(self, *args, **options):
         self.model_name = options["model_name"][0]
         self.field_name = options["field_name"][0]
+        self.force_regen = options.get("force", False)
 
         self.validate_input()
         self.parse_input()
@@ -47,17 +53,60 @@ class Command(BaseCommand):
         # String field name to re-generate.
         self.field = self.field_name
 
+    def get_missing_thumbnails(self, file_field):
+        """
+        Check which thumbnail variations are missing for a given file field.
+        Returns a list of missing thumbnail names.
+        """
+        if not hasattr(file_field, 'field') or not hasattr(file_field.field, 'thumbs'):
+            return []
+
+        missing_thumbs = []
+
+        for thumb in file_field.field.thumbs:
+            thumb_name, thumb_options = thumb
+            thumb_filename = file_field._calc_thumb_filename(thumb_name)
+
+            print(f"checking thumb - ${thumb_filename}")
+
+            # Check if the thumbnail file exists in storage
+
+
+            if not file_field.storage.exists(thumb_filename):
+                print(f"\tmissing thumb - ${thumb_filename}")
+                missing_thumbs.append(thumb_name)
+            else:
+                print(f"\tthumb found - ${thumb_filename}")
+
+        return missing_thumbs
+
+    def needs_regeneration(self, file_field):
+        """
+        Determine if thumbnails need to be regenerated for this file field.
+        Returns True if any thumbnails are missing or if force mode is enabled.
+        """
+        if self.force_regen:
+            return True
+
+        missing_thumbs = self.get_missing_thumbnails(file_field)
+        return len(missing_thumbs) > 0
+
     def regenerate_thumbs(self):
         """
-        Handle re-generating the thumbnails. All this involves is reading the
-        original file, then saving the same exact thing. Kind of annoying, but
-        it's simple.
+        Handle re-generating the thumbnails. Only regenerates when thumbnails
+        are missing or when --force is used.
         """
         Model = self.model
         instances = Model.objects.all()
         num_instances = instances.count()
+
+        processed_count = 0
+        skipped_no_file = 0
+        skipped_exists = 0
+        error_count = 0
+
         # Filenames are keys in here, to help avoid re-genning something that
-        # we have already done.
+        # we have already done in this run.
         regen_tracker = {}
 
         counter = 1
@@ -68,6 +117,7 @@ class Command(BaseCommand):
                     "(%d/%d) ID: %d -- Skipped -- No file"
                     % (counter, num_instances, instance.id)
                 )
+                skipped_no_file += 1
                 counter += 1
                 continue
 
@@ -75,28 +125,47 @@ class Command(BaseCommand):
 
             if file_name in regen_tracker:
                 print(
-                    "(%d/%d) ID: %d -- Skipped -- Already re-genned %s"
+                    "(%d/%d) ID: %d -- Skipped -- Already processed %s"
                     % (counter, num_instances, instance.id, file_name)
                 )
+                skipped_exists += 1
                 counter += 1
                 continue
 
-            # Keep them informed on the progress.
-            print(
-                "(%d/%d) ID: %d -- %s"
-                % (counter, num_instances, instance.id, file_name)
-            )
+            if not self.needs_regeneration(file):
+                missing_thumbs = self.get_missing_thumbnails(file)
+                if not missing_thumbs:
+                    print(
+                        "(%d/%d) ID: %d -- Skipped -- All thumbnails exist for %s"
+                        % (counter, num_instances, instance.id, file_name)
+                    )
+                    skipped_exists += 1
+                    regen_tracker[file_name] = True
+                    counter += 1
+                    continue
+                else:
+                    print(
+                        "(%d/%d) ID: %d -- Processing %s (missing: %s)"
+                        % (counter, num_instances, instance.id, file_name, ", ".join(missing_thumbs))
+                    )
+            else:
+                action = "Force regenerating" if self.force_regen else "Processing"
+                print(
+                    "(%d/%d) ID: %d -- %s %s"
+                    % (counter, num_instances, instance.id, action, file_name)
+                )
 
             try:
                 fdat = file.read()
                 file.close()
                 del file.file
             except IOError:
-                # Key didn't exist.
+                # File doesn't exist in storage
                 print(
-                    "(%d/%d) ID %d -- Error -- File missing on S3"
+                    "(%d/%d) ID %d -- Error -- File missing in storage"
                     % (counter, num_instances, instance.id)
                 )
+                error_count += 1
                 counter += 1
                 continue
 
@@ -105,24 +174,38 @@ class Command(BaseCommand):
             except ValueError:
                 # This field has no file associated with it, skip it.
                 print(
-                    "(%d/%d) ID %d --  Skipped -- No file on field)"
+                    "(%d/%d) ID %d -- Skipped -- No file content"
                     % (counter, num_instances, instance.id)
                 )
+                skipped_no_file += 1
                 counter += 1
                 continue
 
             # Saving pumps it back through the thumbnailer, if this is a
             # ThumbnailField. If not, it's still pretty harmless.
-
             try:
                 file.generate_thumbs(file_name, file_contents)
+                processed_count += 1
             except IOError as e:
                 print(
-                    "(%d/%d) ID %d --  Error -- Image may be corrupt)"
-                    % (counter, num_instances, instance.id)
+                    "(%d/%d) ID %d -- Error -- Image may be corrupt (%s)"
+                    % (counter, num_instances, instance.id, str(e))
                 )
+                error_count += 1
                 counter += 1
                 continue
 
             regen_tracker[file_name] = True
             counter += 1
+
+        print("\nREGENERATION SUMMARY:")
+        print(f"\tTotal instances: {num_instances}")
+        print(f"\tProcessed (regenerated): {processed_count}")
+        print(f"\tSkipped (no file): {skipped_no_file}")
+        print(f"\tSkipped (thumbnails exist): {skipped_exists}")
+        print(f"\tErrors: {error_count}")
+
+        if self.force_regen:
+            print("\nNote: --force was used, all thumbnails were regenerated")
+        else:
+            print("\nNote: Only missing thumbnails were regenerated\n\tUse --force to regenerate all thumbnails")
